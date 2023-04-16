dc8501d4fccbd7205eaec7c4372008511a873159
Mustafa Said Çanak <mscanak0@gmail.com>
2023-04-16 10:28:42 +0300

    2 tane hashmap açtım inputNumber input numberların verilen size a göre hexdec olarak putluyor inputNumber hextodec işlemi için kullanılacak converted dec outputnumber a yazılacak

--- BinaryDataConverter.java
+++ BinaryDataConverter.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class BinaryDataConverter {
@@ -47,37 +48,55 @@ public class BinaryDataConverter {
             }
         }
 
-        String[] dataInCurrentLine = new String[lineCount * 12];
+        int rowCounter = 0;
+        HashMap<String, String> inputNumber = new HashMap<String, String>();
+        HashMap<String, String> outputNumber = new HashMap<String, String>();
+        String[] dataInCurrentLine = new String[12];
+
         while (splitter.hasNextLine()) {
-            //get the current line
+            // get the current line
             String currentLine = splitter.nextLine();
             dataInCurrentLine = currentLine.split(" ");
 
-            // convert from hexadecimal to decimal
-            switch (dataType) {
+            String currentString = "";
+            rowCounter++;
 
-                // nael ve said
-                case "float":
-    
-                    break;
-    
-                // karagul
-                case "int":
-    
-                    break;
-    
-                // kadir
-                case "unsigned":
-    
-                    break;
-    
-                default:
-                    break;
+            for (int i = 0; i < dataInCurrentLine.length; i++) {
+                currentString += dataInCurrentLine[i];
+
+                if (currentString != null && currentString != "" && (i + 1) % dataSize == 0) {
+                    inputNumber.put(rowCounter + ":" + (i + 1) / dataSize, currentString);
+                    currentString = "";
+                }
+
             }
+
         }
 
+        // convert from hexadecimal to decimal
+        switch (dataType) {
+
+            // nael ve said
+            case "float":
+
+                break;
+
+            // karagul
+            case "int":
+
+                break;
+
+            // kadir
+            case "unsigned":
+
+                break;
+
+            default:
+                break;
+        }
         inputReader.close();
         lineCounter.close();
         outputWriter.close();
     }
+
 }
